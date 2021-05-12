f3ae763cbb29339ee6a194d75a5bbf1f24cf1f5d
Conor Ryan <ryan0463@umn.edu>
2021-05-12 12:23:14 -0500

    Loop over runs

--- Probit/Main.jl
+++ Probit/Main.jl
@@ -7,8 +7,7 @@ addprocs(3)
 @everywhere using FiniteDiff
 @everywhere using BenchmarkTools
 
-@everywhere data_file = "choice14_samp5"
-@everywhere haltonDraws = 10000
+
 
 
 @everywhere include("ProbitTypes.jl")
@@ -20,27 +19,38 @@ addprocs(3)
 @everywhere include("Estimate.jl")
 @everywhere include("SpecificationRun.jl")
 
-@everywhere include("Load.jl")
-@everywhere spec=[:adjprem,:plan2,:plan3,:plan4,
-                        :coins,:sclb,:scub,
-                        :plan2_cost,:plan3_cost,:plan4_cost]
+data_file_vec = ["choice14_samp20","choice14_samp10","choice14_samp5"]
+halton_draw_vec = [10000,50000]
+
+for halton_i in halton_draw_vec, data_i in data_file_vec
+
+    @everywhere data_file = $data_i
+    @everywhere haltonDraws = $halton_i
+
+    println("Running Data $data_file with $haltonDraws draws")
+
+    @everywhere include("Load.jl")
+    @everywhere spec=[:adjprem,:plan2,:plan3,:plan4,
+                            :coins,:sclb,:scub,
+                            :plan2_cost,:plan3_cost,:plan4_cost]
 
 
-@everywhere p0 = [-0.0005;.01;.01;.01;.01;.01;.01;
-                        .0001;.0001;.0001;
-                        1.5;2.0;-0.5;0.25;-0.5]
+    @everywhere p0 = [-0.0005;.01;.01;.01;.01;.01;.01;
+                            .0001;.0001;.0001;
+                            1.5;2.0;-0.5;0.25;-0.5]
 
-@everywhere srch_var = [0.001;1;1;1;1;1;1;
-                .001;.001;.001;
-                1;1;1;1;1]
-num_particles = 50
+    @everywhere srch_var = [0.001;1;1;1;1;1;1;
+                    .001;.001;.001;
+                    1;1;1;1;1]
+    num_particles = 50
 
 
 
-@everywhere data = ChoiceData(df,
-                spec=spec,
-                est_draws=haltonDraws)
-@everywhere df = nothing
-@everywhere GC.gc()
-estimate_Model(data,num_particles,p0,srch_var,
-                        "$googleDrivePath/HSA Probit/Results/Results-$datafile-draw$haltonDraws")
+    @everywhere data = ChoiceData(df,
+                    spec=spec,
+                    est_draws=haltonDraws)
+    @everywhere df = nothing
+    @everywhere GC.gc()
+    estimate_Model(data,num_particles,p0,srch_var,
+                            "$googleDrivePath/HSA Probit/Results/Results-$datafile-draw$haltonDraws")
+end
